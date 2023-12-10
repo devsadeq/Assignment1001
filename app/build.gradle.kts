d684f93cf4aad6a33de8c1b4e90d0bd75dc6c767
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-12-10 18:14:19 +0300

    integrate dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -31,17 +32,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -78,6 +79,7 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48.1")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
--- app/src/main/java/com/devsadeq/assignment1001/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/devsadeq/assignment1001/data/repository/ProductRepositoryImpl.kt
@@ -4,7 +4,7 @@ import com.devsadeq.assignment1001.data.repository.mapper.toEntity
 import com.devsadeq.assignment1001.domain.model.Product
 import javax.inject.Inject
 
-class ProductsRepositoryImpl @Inject constructor(
+class ProductRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource
 ) : ProductRepository {
     override suspend fun getProducts(): List<Product> {
